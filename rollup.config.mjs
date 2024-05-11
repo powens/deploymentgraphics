2049b98d73cb2f411226c4fa7de782792c8dcf37
Patrick Owens <1919554+powens@users.noreply.github.com>
2024-05-11 08:35:57 -0700

    Add rollup build

---
+++ rollup.config.mjs
@@ -0,0 +1,16 @@
+import serve from "rollup-plugin-serve";
+import { autoReload } from "rollup-plugin-auto-reload";
+
+export default {
+  input: "src/make-mission-card.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "cjs",
+  },
+  treeshake: false,
+
+  plugins: [
+    serve({ contentBase: ["dist", "static"], open: true }),
+    autoReload(),
+  ],
+};
